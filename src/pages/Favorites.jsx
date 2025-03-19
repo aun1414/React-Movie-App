43afee9b5ed1571829960c1e29c8e2b4eb93a12a
aun1414 <abbasaun1414@gmail.com>
2025-03-19 15:51:53 -0400

    Navbar

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites(){
+     return (
+         <div>
+             <h1>My Favorites</h1>
+             <p>These are my favorite movies</p>
+         </div>
+     )
+}
+
+export default Favorites;
