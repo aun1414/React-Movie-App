4ac13636620adf20c25bce70dcc5e68636bd87a4
aun1414 <abbasaun1414@gmail.com>
2025-03-19 12:48:55 -0400

    Searchquery

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import MovieCard from "../components/MovieCard"
+import { useState } from "react";
 function Home(){
 
+    const [searchQuery,setSearchQuery]=useState("");
     const movies=[
         {id:1,title:"John Wick", release_date:"2020"},
         {id:2,title:"The Godfather", release_date:"1972"},
@@ -8,16 +10,27 @@ function Home(){
         {id:4,title:"Shawshank Redemption", release_date:"1994"},
     ];
 
-    const handleSearch=() =>{
+    const handleSearch=(e) =>{
+        e.preventDefault();
+        alert("Searching for "+searchQuery);
+        setSearchQuery("");
 
     }
     return <div className="home">
         <form onSubmit={handleSearch} className="search-form">
-            <input type="text" placeholder="Search for movies" className="search-input"/>
+            <input 
+            type="text" 
+            placeholder="Search for movies" 
+            className="search-input"
+            value={searchQuery}
+            onChange={(e)=>setSearchQuery(e.target.value)}
+            />
             <button type="submit" className="search-button">Search</button>
+        
         </form>
         <div className="movies-grid">
-            {movies.map(movie=><MovieCard movie={movie} key={movie.id}/>)}
+            {movies.map(movie=> movie.title.toLowerCase().startsWith(searchQuery) && 
+            <MovieCard movie={movie} key={movie.id}/>)}
         </div>
     </div>
 }
